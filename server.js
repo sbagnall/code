8165d1934491838ae3f59e987da5e0a69b61d470
sbagnall <steve.bagnall@gmail.com>
2015-07-12 12:56:37 +0100

    removed custom router and replaced with connect and serve-static

--- server.js
+++ server.js
@@ -1,46 +1,41 @@
 (function () {
 	'use strict';
 	
-	var srv = require('http').createServer(handler),
-	router = require('./src/app/router'),
-	io = require('socket.io')(srv),
-	constants = require('./src/shared/constants'),
-	broker = require('./src/shared/broker'),
-	handlerMappings = require('./src/app/handlerMappings');
+	var connect = require('connect'),
+		app = connect(),
+		serveStatic = require('serve-static'),
+		http = require('http'),
+		server = http.createServer(app),
+		io = require('socket.io')(server),
+		constants = require('./src/shared/constants'),
+		broker = require('./src/shared/broker'),
+		handlerMappings = require('./src/app/handlerMappings');
 
-	function handler (req, res) {
+	app.use(serveStatic(__dirname + '/public'))
+		.use(serveStatic(__dirname + '/node_modules/jquery/dist'))
+		.use(serveStatic(__dirname + '/node_modules/socket.io/node_modules/socket.io-client'));
 
-		var response = router.resolve(req.url, __dirname);
-
-		if (!response) {
-			return;
-		}
-
-		res.writeHead(200, {
-			'Content-Type': response.contentType,
-			'Content-Length': response.content.length 
-		});
-
-		res.end(response.content);
-	}
-
-	srv.listen(3000, function () {
+	server.listen(3000, function () {
 		console.log('listening on http://localhost:3000');
 	});
 
+	server.on('error', function (e) {
+		console.log('error: ' + e)
+	});
+
 	io.on('connection', function (socket) {
-		console.log('server received connection');
+		console.log('server received connection from ' + socket.id);
 
 		socket.on(constants.appName, function (data) {
 			broker(handlerMappings, { io: io }).handleMessage(data);
 		});
 
-		socket.on('disconnect', function (e) {
-			console.log('client disconnected: ' + e);
+		socket.on('disconnect', function () {
+			console.log('client disconnected: ' + socket.id);
 		});
 
-		socket.on('error', function (e) {
-			console.log('error: ' + e);
+		socket.on('error', function (err) {
+			console.log('error on socket ' + socket.id + ': ' + err);
 		});
 
 	});
